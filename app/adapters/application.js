46618770a7cc752c6f53874089804963569752aa
Julie David <julie.angela.david@gmail.com>
2017-01-25 01:11:45 -0500

    Successfully makes CORS request to Meetup API

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -1,5 +1,6 @@
 import DS from 'ember-data';
+import environment from '../config/environment';
 
 export default DS.RESTAdapter.extend({
-  host: 'https://api.meetup.com/Triangle-Ember/events?key=' + ENV.MEETUP_KEY + '&sign=true'
+  host: 'https://api.meetup.com/Triangle-Ember/events?key=' + environment.MEETUP_KEY + '&sign=true'
 });
